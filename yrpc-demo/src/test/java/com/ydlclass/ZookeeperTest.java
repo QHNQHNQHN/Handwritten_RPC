ddba5451dd788bb79822cb495926bfa07846c8f7
Harrison.Qian-钱浩楠 <Harrison.Qian@fanruan.com>
2025-08-11 10:09:28 +0800

    完成 zookeeper 中创建客户端连接以及创建/删除/更新 持久节点的 demo

--- yrpc-demo/src/test/java/com/ydlclass/ZookeeperTest.java
+++ yrpc-demo/src/test/java/com/ydlclass/ZookeeperTest.java
@@ -9,29 +9,34 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
+
 /**
- * @author it楠老师
- * @createTime 2023-06-20
+ * @author QHN
+ * @date 2025/08/11
  */
 public class ZookeeperTest {
 
+    // 创建一个 zooKeeper 变量，存放客户端连接对象
     ZooKeeper zooKeeper;
+    // 创建一个计数器为 1 的1闭锁，用来让主线程等待 zooKeeper 客户端连接成功再继续执行
     CountDownLatch countDownLatch = new CountDownLatch(1);
 
+    // 创建 zooKeeper 客户端连接。
     @Before
     public void createZk(){
 
-        // 定义连接参数
+        // 定义 zooKeeper 服务端的地址 (ip：端口)
         String connectString = "127.0.0.1:2181";
-//        String connectString = "192.168.126.129:2181,192.168.126.132:2181,192.168.126.133:2181";
-        // 定义超时时间
+        /*
+        如果是 zooKeeper 集群，用逗号隔开多个地址
+        String connectString = "192.168.126.129:2181, 192.168.126.132:2181, 192.168.126.133:2181";
+        */
+
+        // 定义连接会话超时时间，单位是毫秒
+        // 如果这个时间内 zookeeper 服务端没有收到客户端的心跳，会认为客户端掉线。
         int timeout = 10000;
 
         try {
-            // new MyWatcher() 默认的watcher
-            // 创建一个zookeeper实例，是否需要时间，是否需要等待
-            // 构建zookeeper是否需要等待连接
-//            zooKeeper = new ZooKeeper(connectString,timeout,new MyWatcher());
             zooKeeper = new ZooKeeper(connectString, timeout, event -> {
                 // 只有连接成功才放行
                 if(event.getState() == Watcher.Event.KeeperState.SyncConnected){
@@ -39,24 +44,34 @@ public class ZookeeperTest {
                     countDownLatch.countDown();
                 }
             });
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    // 测试创建永久节点
     @Test
     public void testCreatePNode(){
         try {
-            // 会等待连接成功
+            // 等待连接成功
             countDownLatch.await();
-            zooKeeper.setData("/ydlclass","hi".getBytes(),-1);
+
+            // 将节点 /ydlclass 的数据改为 “hi”，-1 表示不检查版本号，直接写入
+//            zooKeeper.setData("/ydlclass","hi".getBytes(),-1);
+
+            /*
+             参数 1：要创建的节点路径
+             参数 2：节点保存的数据，必须是字节数组
+             参数 3：访问权限控制。这里表示完全开放权限，任何连接的客户端均可以操作 ZNode
+             参数 4：节点类型：这里表示持久化节点
+             */
             String result = zooKeeper.create("/ydlclass", "hello".getBytes(),
                     ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             System.out.println("result = " + result);
         } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
         } finally {
+            // 如果 zookeeper 不为空，关闭与服务端的连接
             try {
                 if(zooKeeper != null){
                     zooKeeper.close();
@@ -67,10 +82,16 @@ public class ZookeeperTest {
         }
     }
 
+    // 测试删除持久化节点
     @Test
     public void testDeletePNode(){
         try {
-            // version: cas  mysql  乐观锁，  也可以无视版本号  -1
+            /*
+                参数 1：要删除的节点  参数 2：版本号
+                zookeeper 每个节点都有版本号，每次更新数据/结构都会加 1。乐观锁机制。
+                删节点时，传入版本号。zookeeper 先检查版本号是否一致，一致才删除。
+                如果传入的版本号为 -1，表示忽略版本号，直接删除节点。
+             */
             zooKeeper.delete("/ydlclass",-1);
         } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
@@ -88,7 +109,7 @@ public class ZookeeperTest {
     @Test
     public void testExistsPNode(){
         try {
-            // version: cas  mysql  乐观锁，  也可以无视版本号  -1
+            // 检查节点是否存在。存在返回一个 stat 对象，不存在返回 null。
             Stat stat = zooKeeper.exists("/ydlclass", null);
 
             zooKeeper.setData("/ydlclass","hi".getBytes(),-1);
@@ -96,13 +117,12 @@ public class ZookeeperTest {
             // 当前节点的数据版本
             int version = stat.getVersion();
             System.out.println("version = " + version);
-            // 当前节点的acl数据版本
+            // 当前节点的 acl 访问控制列表版本号
             int aversion = stat.getAversion();
             System.out.println("aversion = " + aversion);
             // 当前子节点数据的版本
             int cversion = stat.getCversion();
             System.out.println("cversion = " + cversion);
-
 
         } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
